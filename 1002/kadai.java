8f369ff47b3e6231e21fb92c0049f16e50094087
s18011 <s18011@std.it-college.ac.jp>
2018-10-03 00:43:38 +0000

    kadai

---
+++ 1002/kadai.java
@@ -0,0 +1,9 @@
+public class kadai {
+    public static void main(String[] args) {
+        System.out.println("Hello,World!");
+        System.out.println(" Hello.World!");
+        System.out.println("  Hello.World!");
+        System.out.println("   Hello.World!");
+        System.out.println("    Hello.World!");
+    }
+}
